0bd25d0f06d5b9bc56c82c41be1fd0253931fd48
Student <example@example.com>
2019-11-05 15:21:07 -0800

    add initial files, JS functionality, HTML content and CSS styling.

---
+++ README.md
@@ -0,0 +1,9 @@
+specs:  -
+user selects a movie option
+select time of day
+inputs age, age is sorted into three groups: elderly = > 64
+                                            child = < 13
+                                            adult the else
+runtimes before 18:00 are priced lower than the rest, aka matinee
+Movies will have a different initial cost, depending on how long thy have been out
+price = (radio val)(baseCost) - discount age
---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+
+function Ticket (age, movie, time, cost) {
+  this.age = age,
+  this.movie = movie,
+  this.time = time, //
+  this.cost = cost // a ratio depending on how old the movie is
+}
+function calcCost (time, age, cost){
+  if ((time === 7) && (age <= 13)) {
+    console.log("youngens matinee");
+    return cost - 5
+  } else if ((time === 7) && (age >= 65)) {
+    console.log("oldies matinee");
+    return cost - 6
+  } else if (age <=13) {
+    console.log("youngens")
+    return cost -2
+  } else if (age >= 65) {
+    console.log("oldies")
+    return cost - 3
+  } else if (time === 7) {
+    console.log("matinee");
+    return cost -3
+  } else {
+  return cost
+  }
+  return cost
+}
+
+$(document).ready(function(){
+  $('form#userInput').submit(function (event){
+    event.preventDefault();
+    var userAgeInput = parseInt($('input#user-age').val());
+    var movieTimeInput = parseInt($("input[name='Radio']:checked").val());
+    var movieInput = $("input[name='movie']:checked").val();
+    var price = 10;
+    console.log(movieInput);
+    var myTicket = new Ticket(userAgeInput, movieInput, movieTimeInput, price);
+    console.log(myTicket);
+    console.log(calcCost(movieTimeInput, userAgeInput, price));
+
+  });
+  $('#jokerInput').click(function(){
+    $('#jokerTime').toggle();
+    $('#goneWithTime , #toyStoryTime').hide();
+  });
+  $('#goneWithInput').click(function(){
+    $('#goneWithTime').toggle();
+    $('#jokerTime , #toyStoryTime').hide();
+  });
+  $('#toyStoryInput').click(function(){
+    $('#toyStoryTime').toggle()
+    $('#jokerTime , #goneWithTime').hide();
+
+  });
+});
